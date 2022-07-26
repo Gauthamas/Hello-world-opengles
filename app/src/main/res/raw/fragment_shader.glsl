8012bd8d382e0ca5ba663421923938805c044172
gauthamas <gauthamas@gmail.com>
2022-07-26 19:01:05 +0530

    Aspect ratio as width:height

--- app/src/main/res/raw/fragment_shader.glsl
+++ app/src/main/res/raw/fragment_shader.glsl
@@ -7,6 +7,7 @@ varying vec2 v_TexCoordinate;
 
 vec4 getCurrentColor()
 {
+     //Flipping coordinates because of how images are read in textures
      vec2 flipped_texcoord = vec2(v_TexCoordinate.x, 1.0 - v_TexCoordinate.y);
      return texture2D(u_Texture, flipped_texcoord);
 }
--- app/src/main/res/raw/vertex_shader.glsl
+++ app/src/main/res/raw/vertex_shader.glsl
@@ -8,12 +8,12 @@ varying vec2 v_TexCoordinate;
 
 void main()
 {
-            //Image height/width
-            float aspect = 256.0/512.0;
-            vec2 scale = vec2(1.0/aspect, 1.0);
+            //Image width/height
+            float aspect = 512.0/256.0;
+            vec2 scale = vec2(aspect, 1.0);
             if(u_IsPortrait == 1)
             {
-                scale = vec2(1.0, 1.0/aspect);
+                scale = vec2(1.0, aspect);
             }
             v_TexCoordinate = scale*(a_TexCoordinate - 0.5)+0.5;
             gl_Position = a_Position;
